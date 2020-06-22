6b736fd5cdb785aa13c8d4c4d306bc971c8d7fa5
SdBU034 <zhengtao034@163.com>
2020-06-22 16:32:44 +0800

    添加了通过小点点控制轮播图以及轮播图循环滚动功能

--- front/src/js/index.js
+++ front/src/js/index.js
@@ -1,15 +1,37 @@
 // 面向对象
 
 function Banner() {
+    this.bannerWidth = 800;
     this.bannerGroup = $('#banner-group');
-    this.index = 0;
+    this.index = 1;
     this.leftArrow = $('.left-arrow');
     this.rightArrow = $('.right-arrow');
     this.bannerUl = $('#banner-ul');
     this.liList = this.bannerUl.children('li');
     this.bannerCount = this.liList.length;
-    this.listenBannerHover();
+    this.pageControl = $('.page-control');
 }
+
+Banner.prototype.initBanner = function () {
+    var self = this;
+    var firstBanner = self.liList.eq(0).clone();
+    var lastBanner = self.liList.eq(self.bannerCount - 1).clone();
+    self.bannerUl.append(firstBanner);
+    self.bannerUl.prepend(lastBanner);
+    this.bannerUl.css({'width': this.bannerWidth * (self.bannerCount + 2), 'left': -self.bannerWidth});
+};
+
+Banner.prototype.initPageControl = function () {
+    var self = this;
+    for(var i=0; i<self.bannerCount; i++){
+        var circle = $('<li></li>');
+        self.pageControl.append(circle);
+        if (i === 0){
+            circle.addClass('active');
+        }
+    }
+    self.pageControl.css({'width': 12 * self.bannerCount + 30 * self.bannerCount})
+};
 
 Banner.prototype.toggleArrow = function (isShow) {
     var self = this;
@@ -22,6 +44,55 @@ Banner.prototype.toggleArrow = function (isShow) {
     }
 };
 
+Banner.prototype.animate = function () {
+    var self = this;
+    this.bannerUl.animate({'left': -self.bannerWidth*self.index}, 1000);
+    var index = self.index;
+    if(index === 0){
+        index = self.bannerCount - 1;
+    }else if(index === self.bannerCount + 1){
+        index = 0;
+    }else{
+        index = self.index - 1;
+    }
+    self.pageControl.children('li').eq(index).addClass('active').siblings().removeClass();
+};
+
+Banner.prototype.loop = function () {
+    var self = this;
+    self.timer = setInterval(function () {
+        if(self.index >= self.bannerCount + 1){
+            self.bannerUl.css({'left': -self.bannerWidth});
+            self.index = 2;
+        }else{
+            self.index += 1;
+        }
+        self.animate();
+    }, 2000);
+};
+
+Banner.prototype.listenArrowClick = function () {
+    var self = this;
+    self.leftArrow.click(function () {
+        if(self.index === 0){
+            self.bannerUl.css({'left': -self.bannerCount*self.bannerWidth})
+            self.index = self.bannerCount - 1;
+        }else{
+            self.index--;
+        };
+        self.animate();
+    });
+
+    self.rightArrow.click(function () {
+       if(self.index === self.bannerCount + 1){
+           self.bannerUl.css({'left': -self.bannerWidth});
+           self.index = 2;
+       } else{
+           self.index ++;
+       };
+       self.animate();
+    });
+};
 
 Banner.prototype.listenBannerHover = function () {
     var self = this;
@@ -34,47 +105,23 @@ Banner.prototype.listenBannerHover = function () {
     });
 };
 
-Banner.prototype.animate = function () {
+Banner.prototype.listenPageControl = function () {
     var self = this;
-    this.bannerUl.animate({'left': -800*self.index}, 1000);
-};
-
-Banner.prototype.loop = function () {
-    var self = this;
-    self.timer = setInterval(function () {
-        if(self.index >= 3){
-            self.index = 0;
-        }else{
-            self.index += 1;
-        }
-        self.animate();
-    }, 2000);
-};
-
-Banner.prototype.listenArrowClick = function () {
-    var self = this;
-    self.leftArrow.click(function () {
-        if(self.index === 0){
-            self.index = self.bannerCount - 1;
-        }else{
-            self.index--;
-        };
-        self.animate();
-    });
-
-    self.rightArrow.click(function () {
-       if(self.index === self.bannerCount - 1){
-           self.index = 0;
-       } else{
-           self.index ++;
-       };
-       self.animate();
-    });
+    self.pageControl.children('li').each(function (index, obj) {
+        $(obj).click(function () {
+            self.index = index + 1;
+            self.animate();
+        });
+    })
 };
 
 Banner.prototype.run = function () {
+    this.initBanner();
+    this.initPageControl();
     this.loop();
+    this.listenBannerHover();
     this.listenArrowClick();
+    this.listenPageControl();
 };
 
 $(function () {
