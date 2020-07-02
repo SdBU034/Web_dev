36351fd6219a7ecfb10184a642ac1e8156187f9f
SdBU034 <zhengtao034@163.com>
2020-07-02 16:36:06 +0800

    添加了登录及注册的模态窗口

---
+++ front/src/js/auth.js
@@ -0,0 +1,24 @@
+
+//登录弹出模态窗口
+$(function () {
+   $('#btn').click(function () {
+       $('.mask-wrapper').show();
+   });
+
+   $('.close-btn').click(function () {
+       $('.mask-wrapper').hide();
+   })
+});
+
+$(function () {
+   $('.switch').click(function () {
+       var scrollWrapper = $('.scroll-wrapper');
+       var currentLeft = scrollWrapper.css('left');
+       currentLeft = parseInt(currentLeft);
+       if(currentLeft < 0){
+           scrollWrapper.animate({'left':'0'});
+       }else{
+           scrollWrapper.animate({'left':'-400px'});
+       }
+   });
+});
